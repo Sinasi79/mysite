ae9f52f9ed9c37613c0ffb217b045423e4babced
Sinasi79 <s.sabetimani@gmail.com>
2026-06-04 17:50:25 +0330

    loading images with django
    using static folder to load images on html

--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index_view(request) :
-    return render(request,'home/index.html')
+    return render(request,'index.html')
 
 def about_view(request) :
     return HttpResponse('About page')
