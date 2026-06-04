cd4ade8a6550781535d5499622727254d8cb2088
Sinasi79 <s.sabetimani@gmail.com>
2026-06-04 16:01:38 +0330

    my app
    .

--- website/views.py
+++ website/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def http_test(request) :
-    return HttpResponse('Hello my frienddddd!')
-
 def index_view(request) :
     return HttpResponse('Home page')
 
