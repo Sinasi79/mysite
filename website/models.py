cbb4bb90c816ec2d13d416ff4751691a02cc6fb4
Sinasi79 <s.sabetimani@gmail.com>
2026-07-01 05:03:31 +0330

    making some migrations

--- website/models.py
+++ website/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 255)
+    content = models.TextField()
+    # author
+    # image
+    status = models.BooleanField(default = False)
+    created_date = models.DateTimeField(auto_now_add = True)
+    updated_date = models.DateTimeField(auto_now = True)
+    published_date = models.DateTimeField(null = True)
+
+class Contact(models.Model):
+    name = models.CharField(max_length = 255)
+    email = models.EmailField()
+    subject = models.CharField(max_length = 255)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add = True)
+    updated_date = models.DateTimeField(auto_now = True)
+
+
