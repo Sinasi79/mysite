e568f246d81f77e8a6f921838bd7614ee4511cdf
Sinasi79 <s.sabetimani@gmail.com>
2026-07-04 14:09:01 +0330

    creating image and author field

--- website/models.py
+++ website/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length = 255)
     content = models.TextField()
-    # author
-    # image
+    author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    image = models.ImageField(upload_to='projects/',default='projects/default.jpg')
     status = models.BooleanField(default = False)
     created_date = models.DateTimeField(auto_now_add = True)
     updated_date = models.DateTimeField(auto_now = True)
